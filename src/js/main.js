2c61efddacf1a7e373dca538c2fefdc944bd035f
PoleshukMax <maxpoleshuk03@gmail.com>
2019-11-26 18:41:50 +0300

    add slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,4 @@
-// Модальное окно
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
-//Меню
-
-var menu = document.getElementById('myMenu');
-menu.onclick = function showMenu() {
-  if(menu.className === "menu") {
-      menu.className += " responsive";
-  } else {
-      menu.className = 'menu';
-  }
-}
+//= "base"
 
 
 
