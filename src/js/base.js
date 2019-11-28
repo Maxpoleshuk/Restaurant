c2416bdcb556ecbd1db010ed67e57e7546a05d3e
PoleshukMax <maxpoleshuk03@gmail.com>
2019-11-28 21:09:45 +0300

    add buttonUp

--- src/js/base.js
+++ src/js/base.js
@@ -1,14 +1,47 @@
+//Кнопка вверх
+window.onload = function () {
+
+    var scrolled;
+    var timer;
+
+    document.getElementById('up').onclick = function () {
+        scrolled = window.pageYOffset;
+
+        scrollToTop()
+    }
+
+
+    function scrollToTop() {
+        if (scrolled > 0) {
+            window.scrollTo(0, scrolled);
+            scrolled = scrolled - 3; //скорость прокрутки
+            timer = setTimeout(scrollToTop, 1);
+        } else {
+            clearTimeout(timer);
+            window.scrollTo(0, 0);
+
+        }
+    }
+    // if (scrolled > 300) {
+    //     document.getElementById('up').style.display = 'block';
+    // } else {
+    //     document.getElementById('up').style.display = 'none';
+    // }
+
+}
+
+
 // Модальное окно
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
+btn.onclick = function () {
     modal.style.display = "block";
 }
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
@@ -18,20 +51,20 @@ window.onclick = function(event) {
 
 var menu = document.getElementById('myMenu');
 menu.onclick = function showMenu() {
-  if(menu.className === "menu") {
-      menu.className += " responsive";
-  } else {
-      menu.className = 'menu';
-  }
+    if (menu.className === "menu") {
+        menu.className += " responsive";
+    } else {
+        menu.className = 'menu';
+    }
 }
 //Карусель
- var slideIndex = 1;
- showSlides(slideIndex);
+var slideIndex = 1;
+showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
- 
+
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
@@ -41,19 +74,19 @@ function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
 
-    if (n >slides.length) {
+    if (n > slides.length) {
         slideIndex = 1
     }
-    if (n < 1){
+    if (n < 1) {
         slideIndex = slides.length
     }
-    for (i = 0; i < slides.length; i++){
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++){
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace("active", "");
 
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += "active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += "active";
 }
